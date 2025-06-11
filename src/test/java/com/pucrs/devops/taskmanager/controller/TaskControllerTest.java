caf7ab7c626aa4e060c46dc08de507d814bcbf4b
jvitorbarbosa <jvitorbf35@gmail.com>
2025-06-10 22:31:56 -0300

    test: generate base config for integration tests

---
+++ src/test/java/com/pucrs/devops/taskmanager/controller/TaskControllerTest.java
@@ -0,0 +1,7 @@
+package com.pucrs.devops.taskmanager.controller;
+
+import com.pucrs.devops.taskmanager.config.BaseIntegrationTest;
+
+public class TaskControllerTest extends BaseIntegrationTest {
+
+}
